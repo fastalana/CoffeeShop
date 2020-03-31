373582e6b6be268151b3a97d0184b6c763b42e9a
Alana Bellucci <fastalana@gmail.com>
2020-03-31 09:27:26 -0600

    Implemented and tested the /drinks/<id> DELETE route.

--- backend/src/api.py
+++ backend/src/api.py
@@ -102,9 +102,25 @@ def update_drink(token, id):
     returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
         or appropriate status code indicating reason for failure
 '''
-# @app.route('/drinks/<int:drink_id>', methods=['DELETE'])
-# @requires_auth('delete:drinks')
-# def delete_drink(token):
+@app.route('/drinks/<id>', methods=['DELETE'])
+@requires_auth('delete:drinks')
+def delete_drink(token, id):
+    drink = Drink.query.filter(Drink.id == id).one_or_none()
+    try:
+        if drink is None:
+            abort(404)
+        
+        drink.delete()
+
+        return jsonify({
+            'success': True,
+            'delete': id
+        })
+    
+    except:
+        abort(422)
+
+
 
 
 ## Error Handling
